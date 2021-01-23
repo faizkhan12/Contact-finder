49d82661ad77f40d2ae536483ac4539a18f5468b
faizkhan12 <faizk6797@gmail.com>
2021-01-23 20:57:52 +0530

    Start front end design with reducer and context

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,69 @@
+import React, { useReducer } from 'react'
+import uuid from 'uuid'
+import ContactContext from './ContactContext'
+import contactReducer from './ContactReducer'
+import {
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACT,
+    CLEAR_FILTER,
+} from '../types'
+
+const ContactState = (props) => {
+    const initialState = {
+        contacts: [
+            {
+                id: 1,
+                name: 'Jill Johnson',
+                email: 'jill@gmail.com',
+                phone: '111-111-1111',
+                type: 'personal',
+            },
+            {
+                id: 2,
+                name: 'Sara Watson',
+                email: 'sara@gmail.com',
+                phone: '112-111-1111',
+                type: 'personal',
+            },
+            {
+                id: 1,
+                name: 'Harry Johnson',
+                email: 'harry@gmail.com',
+                phone: '113-111-1111',
+                type: 'professional',
+            },
+        ]
+    }
+
+    const [state, dispatch] = useReducer(contactReducer, initialState)
+
+    // Add Contact
+
+    // Delete Contact
+
+    // set current Contact
+
+    // clear current Contact
+
+    // update contact
+
+    // filter contacts
+
+    // clear Filter
+
+    return (
+        <ContactContext.Provider
+            value={{
+                contacts: state.contacts
+
+            }}>
+            { props.children}
+        </ContactContext.Provider>
+    )
+}
+
+export default ContactState
