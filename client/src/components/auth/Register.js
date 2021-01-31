e83238abd331e4af2cd993c8d0139a1420936ac6
faizkhan12 <faizk6797@gmail.com>
2021-01-31 21:49:37 +0530

    Add alert context

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import AlertContext from '../../context/alert/AlertContext'
 
 const Register = () => {
+    const alertContext = useContext(AlertContext)
+
+    const { setAlert } = alertContext
     const [user, setUser] = useState({
         name: '',
         email: '',
@@ -14,7 +18,13 @@ const Register = () => {
 
     const onSubmit = (e) => {
         e.preventDefault()
-        console.log('Register')
+        if (name === '' || email === '' || password === '') {
+            setAlert('Please enter all fields', 'danger')
+        } else if (password !== password2) {
+            setAlert('Password does not match', 'danger')
+        } else {
+            console.log('Register')
+        }
     }
 
     return (
@@ -30,6 +40,7 @@ const Register = () => {
                         name='name'
                         value={name}
                         onChange={onChange}
+                        required
                     />
                 </div>
                 <div className='form-group'>
@@ -39,6 +50,7 @@ const Register = () => {
                         name='email'
                         value={email}
                         onChange={onChange}
+                        required
                     />
                 </div>
                 <div className='form-group'>
@@ -48,6 +60,8 @@ const Register = () => {
                         name='password'
                         value={password}
                         onChange={onChange}
+                        required
+                        minLength="6"
                     />
                 </div>
                 <div className='form-group'>
@@ -57,6 +71,8 @@ const Register = () => {
                         name='password2'
                         value={password2}
                         onChange={onChange}
+                        required
+                        minLength="6"
                     />
                 </div>
                 <input
